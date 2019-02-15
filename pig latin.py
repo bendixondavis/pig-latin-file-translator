ec8cc765f06e15c24803bfc7e252d64d21f58d6b
Ben Davis <bendixondavis@gmail.com>
2019-02-15 10:44:54 -0500

    Update pig latin.py
    Got insertion of punctuation working all the way! working correctly in command line input of text version

--- pig latin.py
+++ pig latin.py
@@ -1,8 +1,9 @@
 import re
 
 #returns tuple of the punctuation character found and the position it was found at or -1 if not found
-def search_for_punctuation(word):
-    m = re.search(r'[.,?!:;()"“”]', word)
+def search_for_punctuation(word, pos = 0):
+    pattern = re.compile(r'[.,?!:;()"“”]')
+    m = pattern.search(word, pos)
     if m != None:
         mString = m.group()
         mPos = m.start()
@@ -10,6 +11,7 @@ def search_for_punctuation(word):
     else:
         return -1
 
+#this function takes a string as an input and parses it into pig latin, accounting for any punctuation and returns it
 def pigWord(word):
     punctList = []
     letter_list = list(word)
@@ -17,20 +19,23 @@ def pigWord(word):
     #store it in a separate list to be reattached to the word after parsing into pig latin
     while search_for_punctuation(word) != -1:
         letter_list = list(word)
-        result = search_for_punctuation(word)
+        result = search_for_punctuation(word,-1)
         punctList.append(result)
+
         letter_list.pop(result[1])
         word = "".join(letter_list)
 
-    print(punctList)
-    print(word)
+    #for debugging
+    #print(punctList)
+    #print(word)
+
     #this block of code actually turns the word into pig latin
     firstLetter = letter_list.pop(0)
     letter_list.insert(len(letter_list), firstLetter)
     letter_list.append('a')
     letter_list.append('y')
 
-    for item in punctList:
+    for item in punctList: #insert punctuation into correct spot in string
         if item[1] > (len(letter_list) / 2): #if index of punctuation is more than halfway through word
             letter_list.insert(item[1] + 2 + len(punctList), item[0]) #offset the index by 2 to account for 2 new letters
         else:
